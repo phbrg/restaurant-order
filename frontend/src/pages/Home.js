8a7b8b045e4ac4f68de63c4716ad1ff77b9ff243
NotHiaki <celsohenrique367@gmail.com>
2023-12-13 21:19:52 -0300

    feat: home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,16 @@
+import { useState } from "react"
+
 const Home = () => {
+  const [form, setForm] = useState();
+
   return (
-    <div className="text-red">Home1</div>
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-background">
+    <form className="flex flex-col gap-4 w-64">
+      <input type="text" placeholder="Digite o nome" className="p-2 text-sm font-semibold rounded-md border-none outline-none" />
+      <input type="text" placeholder="Digite a mesa" className="p-2 text-sm font-semibold rounded-md border-none outline-none" />
+      <input type="submit" value="Ir pra o menu" className="p-2 text-sm w-full font-semibold bg-yellow text-white font-semibol border-none rounded-md cursor-pointer" />
+    </form>
+  </div>
   )
 }
 
