7c08b4e131202908ab8391b56be6b6a23b809c61
phpedro06 <pedrohenriquebatistabergamin@gmail.com>
2023-12-13 21:27:52 -0300

    values

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,14 @@
 import { useState } from "react"
 
 const Home = () => {
-  const [form, setForm] = useState();
+  const [name, setName] = useState(null);
+  const [table, setTable] = useState(null);
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center bg-background">
     <form className="flex flex-col gap-4 w-64">
-      <input type="text" placeholder="Digite o nome" className="p-2 text-sm font-semibold rounded-md border-none outline-none" />
-      <input type="text" placeholder="Digite a mesa" className="p-2 text-sm font-semibold rounded-md border-none outline-none" />
+      <input type="text" placeholder="Digite o nome" className="p-2 text-sm font-semibold rounded-md border-none outline-none" value={name} onChange={(e) => setName(e.target.value)} />
+      <input type="text" placeholder="Digite a mesa" className="p-2 text-sm font-semibold rounded-md border-none outline-none" value={table} onChange={(e) => setTable(e.target.value)} />
       <input type="submit" value="Ir pra o menu" className="p-2 text-sm w-full font-semibold bg-yellow text-white font-semibol border-none rounded-md cursor-pointer" />
     </form>
   </div>
