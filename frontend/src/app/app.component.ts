1a889834d4f5f4ac342a1455c71598c0ec8e5095
NotHiaki <celsohenrique367@gmail.com>
2024-01-12 18:49:53 -0300

    feat: formbuilde home form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  formGroup!: FormGroup;
+
+  protected formBuilder = inject(FormBuilder);
+  
+  constructor() {
+    this.formGroup = this.formBuilder.group({
+      name: '',
+      table: ''
+    });
+  }
+
   sendForm(): void {
-    console.log("form invited");
+    console.log(this.formGroup.value);
   }
 }
