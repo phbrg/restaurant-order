6e1041913284fae370291ad3b6a0cd13f941db76
NotHiaki <celsohenrique367@gmail.com>
2024-01-17 17:37:21 -0300

    feat: put cards in menu

--- frontend/src/app/components/menu/menu.component.ts
+++ frontend/src/app/components/menu/menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "./card/card.component";
 
 @Component({
-  selector: 'app-menu',
-  standalone: true,
-  imports: [],
-  templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+    selector: 'app-menu',
+    standalone: true,
+    templateUrl: './menu.component.html',
+    styleUrl: './menu.component.scss',
+    imports: [CardComponent]
 })
 export class MenuComponent {
 
